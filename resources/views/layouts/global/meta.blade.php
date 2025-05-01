d76d9d87634e1167f5543563b8a59570fa2e70e4
Vio-Shn <77569725+Vio-Shn@users.noreply.github.com>
2025-05-01 22:37:17 +0700

    add home background

--- resources/views/layouts/global/meta.blade.php
+++ resources/views/layouts/global/meta.blade.php
@@ -8,3 +8,4 @@
 <link rel="apple-touch-icon" sizes="180x89" href="{{ asset('apple-touch-icon.png') }}">
 <link rel="icon" type="image/png" sizes="32x16" href="{{ asset('favicon-32x32.png') }}">
 <link rel="icon" type="image/png" sizes="16x8" href="{{ asset('favicon-16x16.png') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/dashboard.css') }}">
